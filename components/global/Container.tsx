0e6140c8461f7ecc132c22d5e2e762f643648394
inga-divra <ingady@gmail.com>
2024-09-25 10:16:21 +0300

    Set up Navbar Component and Updated layout.tsx.
    - Integrated components: Logo, LinksDropdown, DarkMode, CartButton, and NavSearch, all wrapped in Container for responsive layout in the navbar.
    
    -Integrated Navbar component and wrapped page content in a Container component with padding for consistent spacing across the layout.

--- components/global/Container.tsx
+++ components/global/Container.tsx
@@ -1,17 +1,17 @@
 import { cn } from '@/lib/utils';
 
-const Container = ({
+function Container({
   children,
   className,
 }: {
   children: React.ReactNode;
   className?: string;
-}) => {
+}) {
   return (
-    <div className={(cn('mx-auto max-w-6xl xl:max-w-7xl px-8'), className)}>
+    <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
       {children}
     </div>
   );
-};
+}
 
 export default Container;
