4f7abeeb873337885f15d711f667244c1e65547c
inga-divra <ingady@gmail.com>
2024-10-01 11:54:17 +0300

    Enhanced the UserIcon component by adding logic to display the user's profile image if available, or a default icon when the image is missing
    - Integrated Clerk's currentUser function to fetch the user's profile details.
    - Added conditional rendering to show either the user's profile image or the default LuUser2 icon with customized styles.

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,17 @@
-const UserIcon = () => {
-  return <div>UserIcon</div>;
+import { LuUser2 } from 'react-icons/lu';
+import { currentUser, auth } from '@clerk/nextjs/server';
+
+const UserIcon = async () => {
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+  if (profileImage) {
+    return (
+      <img className='w-6 h-6 rounded-full object-cover' src={profileImage} />
+    );
+  }
+
+  return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
 };
 
 export default UserIcon;
