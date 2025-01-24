2172746ba0b4ba347adea6317961e47f86c3f2d6
inga-divra <ingady@gmail.com>
2025-01-24 19:19:34 +0200

    Implemented logic for displaying user reviews on ReviewsPage, including fetching reviews and adding delete functionality

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -9,7 +9,37 @@ const ReviewsPage = async () => {
   if (reviews.length === 0)
     return <SectionTitle text='you have no reviews yet' />;
 
-  return <div>ReviewsPage</div>;
+  return (
+    <>
+      <SectionTitle text='Your Reviews' />
+      <section className='grid md:grid-cols-2 gap-8 mt-4 '>
+        {reviews.map((review) => {
+          const { comment, rating } = review;
+          const { name, image } = review.product;
+          const reviewInfo = {
+            comment,
+            rating,
+            name,
+            image,
+          };
+          return (
+            <ReviewCard key={review.id} reviewInfo={reviewInfo}>
+              <DeleteReview reviewId={review.id} />
+            </ReviewCard>
+          );
+        })}
+      </section>
+    </>
+  );
+};
+
+const DeleteReview = ({ reviewId }: { reviewId: string }) => {
+  const deleteReview = deleteReviewAction.bind(null, { reviewId });
+  return (
+    <FormContainer action={deleteReview}>
+      <IconButton actionType='delete' />
+    </FormContainer>
+  );
 };
 
 export default ReviewsPage;
