fff84ffee0f250067a744016b1b250b5bd28a969
inga-divra <ingady@gmail.com>
2025-01-24 19:04:35 +0200

    Setup "reviews" link in utils/links.ts and created reviews page with loading state in app/reviews

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,4 +1,14 @@
-const ReviewsPage = () => {
+import { deleteReviewAction, fetchProductReviewsByUser } from '@/utils/actions';
+import ReviewCard from '@/components/reviews/ReviewCard';
+import SectionTitle from '@/components/global/SectionTitle';
+import FormContainer from '@/components/form/FormContainer';
+import { IconButton } from '@/components/form/Buttons';
+
+const ReviewsPage = async () => {
+  const reviews = await fetchProductReviewsByUser();
+  if (reviews.length === 0)
+    return <SectionTitle text='you have no reviews yet' />;
+
   return <div>ReviewsPage</div>;
 };
 
